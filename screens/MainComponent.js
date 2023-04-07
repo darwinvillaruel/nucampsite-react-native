45d071c4d99bfa5fe92f621bc48b0b3d2cc30718
Darwin <darwinvillaruel2215@gmail.com>
2023-04-07 16:00:19 +0800

    Favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -20,6 +20,7 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
+import FavoriteScreen from './FavoriteScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -117,6 +118,29 @@ const ReservationNavigator = () => {
           headerLeft: () => (
             <Icon
               name='tree'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const FavoritesNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Favorites'
+        component={FavoriteScreen}
+        options={({ navigation }) => ({
+          title: 'Favorite Campsites',
+          headerLeft: () => (
+            <Icon
+              name='heart'
               type='font-awesome'
               iconStyle={styles.stackIcon}
               onPress={() => navigation.toggleDrawer()}
@@ -231,6 +255,22 @@ const Main = () => {
             drawerIcon: ({ color }) => (
               <Icon
                 name='tree'
+                type='font-awesome'
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name='Favorites'
+          component={FavoritesNavigator}
+          options={{
+            title: 'My Favorites',
+            drawerIcon: ({ color }) => (
+              <Icon
+                name='heart'
                 type='font-awesome'
                 size={24}
                 iconStyle={{ width: 24 }}
