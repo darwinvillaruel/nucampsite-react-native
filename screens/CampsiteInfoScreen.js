e35eb95ca009f0f10e442ecb07304c916c01137f
Darwin <darwinvillaruel2215@gmail.com>
2023-04-10 13:41:45 +0800

    Animatable

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -5,6 +5,7 @@ import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { useState } from 'react';
 import { Rating, Input, Icon } from 'react-native-elements';
 import { postComment } from '../features/comments/commentsSlice';
+import * as Animatable from 'react-native-animatable';
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
@@ -59,7 +60,10 @@ const CampsiteInfoScreen = ({ route }) => {
   };
 
   return (
-    <>
+    <Animatable.View
+      animation='fadeInUp'
+      duration={2000}
+      delay={1000}>
       <FlatList
         data={comments.commentsArray.filter(
           (comment) => comment.campsiteId === campsite.id
@@ -138,7 +142,7 @@ const CampsiteInfoScreen = ({ route }) => {
           </View>
         </View>
       </Modal>
-    </>
+    </Animatable.View>
   );
 };
 
