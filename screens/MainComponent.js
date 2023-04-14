2dcc408c9b1301b5a1bda31d331c235962b9d021
Darwin <darwinvillaruel2215@gmail.com>
2023-04-14 11:19:48 +0800

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -21,6 +21,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
 import FavoriteScreen from './FavoriteScreen';
+import LoginScreen from './LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -152,6 +153,28 @@ const FavoritesNavigator = () => {
   );
 };
 
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Login'
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name='sign-in'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const ContactNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -215,6 +238,21 @@ const Main = () => {
         initialRouteName='Home'
         drawerContent={CustomDrawerContent}
         drawerStyle={{ backgroundColor: '#CEC8FF' }}>
+        <Drawer.Screen
+          name='Login'
+          component={LoginNavigator}
+          options={{
+            drawerIcon: ({ color }) => (
+              <Icon
+                name='sign-in'
+                type='font-awesome'
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
         <Drawer.Screen
           name='Home'
           component={HomeNavigator}
